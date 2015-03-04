ff8b9a6f3addeb27e7ef8c1a33f8190299b87529
Jakub Pastuszek <jpastuszek@gmail.com>
2015-03-04 22:57:43 +0000

    logger

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,21 @@
 extern crate clap;
 use clap::{Arg, App};
 
+#[macro_use]
+extern crate log;
+extern crate env_logger;
+
 fn main() {
+	env_logger::init().unwrap();
+
 	let matches = App::new("MyApp")
-        .version("0.0")
-        .author("JP")
-        .about("Does awesome things")
-        .arg(Arg::new("output")
+		.version("0.0")
+		.author("JP")
+		.about("Does awesome things")
+		.arg(Arg::new("output")
 			.help("Name")
 			.index(1)
-		)
-        .get_matches();
+		).get_matches();
 
-    println!("Hello, world! {:?}", matches.value_of("output").unwrap_or(&"nothing".to_string()));
+	println!("Hello, world! {:?}", matches.value_of("output").unwrap_or(&"nothing".to_string()));
 }
