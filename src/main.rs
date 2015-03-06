516bcdde4db7f048b658d8e4d8298b8d3c9244f3
Jakub Pastuszek <jpastuszek@gmail.com>
2015-03-06 22:43:28 +0000

    msgpack and nanomsg

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,10 @@ use clap::{Arg, App};
 #[macro_use]
 extern crate log;
 extern crate env_logger;
+extern crate msgpack;
+extern crate nanomsg;
+
+use nanomsg::{Socket, NanoResult, Protocol};
 
 fn main() {
 	env_logger::init().unwrap();
@@ -18,4 +22,15 @@ fn main() {
 		).get_matches();
 
 	println!("Hello, world! {:?}", matches.value_of("output").unwrap_or(&"nothing".to_string()));
+
+  let arr = vec!["str1".to_string(), "str2".to_string()];
+  let str = msgpack::Encoder::to_msgpack(&arr).ok().unwrap();
+  println!("Encoded: {:?}", str);
+
+  let mut socket = Socket::new(Protocol::Pull).unwrap();
+	let mut endpoint = socket.bind("tcp://*:1112").unwrap();
+
+  let msg = socket.read_to_string().unwrap();
+  // /usr/local/Cellar/nanomsg/0.5-beta/bin/nanocat --verbose --connect-local 1112 --send-timeout 2 --data hello --push
+	println!("We got a message: {}", &*msg);
 }
