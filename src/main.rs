caf7f3d7056c378a09f12dd07cd66cc3b7dcb11a
Jakub Pastuszek <jpastuszek@gmail.com>
2015-03-07 13:18:31 +0000

    trying out structs

--- src/main.rs
+++ src/main.rs
@@ -6,8 +6,28 @@ extern crate log;
 extern crate env_logger;
 extern crate msgpack;
 extern crate nanomsg;
+extern crate chrono;
 
 use nanomsg::{Socket, NanoResult, Protocol};
+
+use std::any::Any;
+use chrono::*;
+use std::ops::*;
+
+struct RawDataPoint<V> {
+	location: String,
+	path: String,
+	component: String,
+	time_stamp: DateTime<UTC>,
+	value: V,
+}
+
+impl RawDataPoint<i32> {
+	fn to_msgpack(&self) -> Vec<u8> {
+		let arr = vec![self.location.clone()];
+	  msgpack::Encoder::to_msgpack(&arr).ok().unwrap()
+	}
+}
 
 fn main() {
 	env_logger::init().unwrap();
@@ -27,10 +47,30 @@ fn main() {
   let str = msgpack::Encoder::to_msgpack(&arr).ok().unwrap();
   println!("Encoded: {:?}", str);
 
-  let mut socket = Socket::new(Protocol::Pull).unwrap();
-	let mut endpoint = socket.bind("tcp://*:1112").unwrap();
+  let data = RawDataPoint {
+  	location: "myhost".to_string(), 
+  	path: "sys/cpu/usage".to_string(), 
+  	component: "user".to_string(), 
+  	time_stamp: UTC::now(),
+  	value: 0.3
+  };
 
-  let msg = socket.read_to_string().unwrap();
+  let data2 = RawDataPoint {
+  	location: "myhost".to_string(), 
+  	path: "sys/cpu/usage".to_string(), 
+  	component: "user".to_string(), 
+  	time_stamp: UTC::now(),
+  	value: 1
+  };
+
+  println!("Encoded: {:?}", data2.to_msgpack());
+  let dec: Vec<String> = msgpack::from_msgpack(data2.to_msgpack().as_mut_slice()).ok().unwrap();
+  println!("Decode: {}", dec[0]);
+
+ // let mut socket = Socket::new(Protocol::Pull).unwrap();
+	//let mut endpoint = socket.bind("tcp://*:1112").unwrap();
+
+ // let msg = socket.read_to_string().unwrap();
   // /usr/local/Cellar/nanomsg/0.5-beta/bin/nanocat --verbose --connect-local 1112 --send-timeout 2 --data hello --push
-	println!("We got a message: {}", &*msg);
+//	println!("We got a message: {}", &*msg);
 }
