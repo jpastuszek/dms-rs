845ea1ce6f4066f4b119efe2d7d48f3b3bb337c9
Jakub Pastuszek <jpastuszek@gmail.com>
2015-03-13 20:55:51 +0000

    using Encodable trait manually

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 #![feature(old_io)]
+#![feature(rustc_private)]
 extern crate clap;
-extern crate serialize;
+//extern crate serialize;
 use clap::{Arg, App};
 
 #[macro_use]
@@ -9,6 +10,9 @@ extern crate env_logger;
 extern crate msgpack;
 extern crate nanomsg;
 extern crate chrono;
+extern crate serialize;
+//extern crate "rustc-serialize" as serialize;
+use serialize::{Encodable, Encoder, Decodable};
 
 use nanomsg::{Socket, NanoResult, Protocol};
 
@@ -17,16 +21,35 @@ use std::ops::*;
 use std::old_io::*;
 use std::collections::HashMap;
 use std::any::Any;
-use serialize::{Encodable, Decodable};
 
-
-#[derive(Encodable,Decodable,PartialEq,Debug)]
+//#[derive(RustcEncodable,RustcDecodable,PartialEq,Debug)]
+//#[derive(Encodable,Decodable,PartialEq,Debug)]
 struct RawDataPoint<V> {
 	location: String,
 	path: String,
 	component: String,
 	//time_stamp: DateTime<UTC>,
 	value: V,
+}
+
+impl Encodable for RawDataPoint<i32> {
+	fn encode<S: Encoder>(&self, s: &mut S) -> Result<(), S::Error> {
+		s.emit_map(4, |s| {
+			try!(s.emit_map_elt_key(0, |s| "location".encode(s)));
+			try!(s.emit_map_elt_val(0, |s| self.location.encode(s)));
+
+			try!(s.emit_map_elt_key(1, |s| "path".encode(s)));
+			try!(s.emit_map_elt_val(1, |s| self.path.encode(s)));
+
+			try!(s.emit_map_elt_key(2, |s| "component".encode(s)));
+			try!(s.emit_map_elt_val(2, |s| self.component.encode(s)));
+
+			try!(s.emit_map_elt_key(3, |s| "value".encode(s)));
+			try!(s.emit_map_elt_val(4, |s| self.value.encode(s)));
+
+			Ok(())
+		})
+	}
 }
 
 impl RawDataPoint<i32> {
@@ -70,11 +93,10 @@ fn main() {
   	value: 1
   };
 
-  println!("Encoded: {:?}", data2.to_msgpack());
   match data2.to_msgpack() {
   		Ok(data2) => {
-				let dec: RawDataPoint<i32> = msgpack::from_msgpack(&data2).ok().unwrap();
-				println!("Decode: {:?}", dec);
+				//let dec: RawDataPoint<i32> = msgpack::from_msgpack(&data2).ok().unwrap();
+				println!("Encoded: {:?}", data2);
   		}
   		Err(error) => {
   			error!("Failed to encode data: {}", error)
@@ -88,3 +110,8 @@ fn main() {
   // /usr/local/Cellar/nanomsg/0.5-beta/bin/nanocat --verbose --connect-local 1112 --send-timeout 2 --data hello --push
 //	println!("We got a message: {}", &*msg);
 }
+
+/*
+2.2.0 :008 > MessagePack.load [132, 168, 108, 111, 99, 97, 116, 105, 111, 110, 166, 109, 121, 104, 111, 115, 116, 164, 112, 97, 116, 104, 173, 115, 121, 115, 47, 99, 112, 117, 47, 117, 115, 97, 103, 101, 169, 99, 111, 109, 112, 111, 110, 101, 110, 116, 164, 117, 115, 101, 114, 165, 118, 97, 108, 117, 101, 208, 1].pack('c*')
+ => {"location"=>"myhost", "path"=>"sys/cpu/usage", "component"=>"user", "value"=>1}
+*/
