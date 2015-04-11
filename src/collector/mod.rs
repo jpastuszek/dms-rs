803802516834ec5bc93643d67d8c1ab8dd83525f
Jakub Pastuszek <jpastuszek@gmail.com>
2015-04-11 21:33:29 +0100

    sinding nanomsg from collector; getting nanomsg related crashes...

--- src/collector/mod.rs
+++ src/collector/mod.rs
@@ -1,16 +1,11 @@
 use nanomsg::{Socket, Protocol};
 use chrono::*;
 
-use capnp::serialize_packed;
-use capnp::{MessageBuilder, MallocMessageBuilder};
-
-use std::io::*;
-
 use std::thread;
 use std::thread::JoinGuard;
 use std::sync::mpsc::sync_channel;
 use std::sync::mpsc::{Receiver, SyncSender};
-use messaging::{RawDataPoint, DataValue};
+use messaging::*;
 
 pub struct CollectorThread<'a> {
     // NOTE: this needs to be before thread so channel is dropped before we join tread
@@ -26,7 +21,7 @@ impl<'a> CollectorThread<'a> {
 
         let thread = thread::scoped(move || {
             let mut socket = Socket::new(Protocol::Push).ok().expect("Cannot create push socket!");
-            let endpoint = match socket.connect(data_processor_address) {
+            let _ = match socket.connect(data_processor_address) {
                 Ok(ep) => ep,
                 Err(error) => panic!("Failed to connect data processor at '{}': {}", data_processor_address, error)
             };
@@ -34,39 +29,7 @@ impl<'a> CollectorThread<'a> {
             loop {
                 match rx.recv() {
                     Ok(raw_data_point) => {
-                        let mut message = MallocMessageBuilder::new_default();
-                        {
-                            let mut raw_data_point_builder = message.init_root::<super::raw_data_point_capnp::raw_data_point::Builder>();
-
-                            raw_data_point_builder.set_location(&*raw_data_point.location);
-                            raw_data_point_builder.set_path(&*raw_data_point.path);
-                            raw_data_point_builder.set_component(&*raw_data_point.component);
-
-                            {
-                                let mut date_time_builder = raw_data_point_builder.borrow().init_timestamp();
-                                date_time_builder.set_unix_timestamp(raw_data_point.timestamp.timestamp());
-                                date_time_builder.set_nanosecond(raw_data_point.timestamp.nanosecond());
-                            }
-
-                            {
-                                let mut _value = raw_data_point_builder.borrow().init_value();
-                                match raw_data_point.value {
-                                    DataValue::Integer(value) => {
-                                        _value.set_integer(value);
-                                    },
-                                    DataValue::Float(value) => {
-                                        _value.set_float(value);
-                                    },
-                                    DataValue::Bool(value) => {
-                                        _value.set_boolean(value);
-                                    },
-                                    DataValue::Text(value) => {
-                                        _value.set_text(&*value);
-                                    },
-                                }
-                            }
-                        }
-                        // write output
+                        socket.send_message("".to_string(), &*raw_data_point, Encoding::Capnp).unwrap();
                     },
                     Err(error) => {
                         info!("Collector thread finished: {}", error);
@@ -116,24 +79,42 @@ impl Collector {
     }
 }
 
-/*
-describe! collector_thread {
-    it "should shut down after going out of scope" {
-        {
-            let _ = CollectorThread::spawn("ipc:///tmp/test.ipc");
+#[cfg(test)]
+mod test {
+    pub use super::*;
+    pub use nanomsg::{Socket, Protocol};
+    pub use messaging::DataValue;
+    pub use std::io::Read;
+
+    describe! collector_thread {
+        before_each {
+            let mut pull = Socket::new(Protocol::Pull).unwrap();
+            let _ = pull.bind("ipc:///tmp/test-collector.ipc").unwrap();
         }
-        assert!(true);
-    }
 
-    describe! collector {
-         it "should pass data points to nanosmg pull socket" {
-            let collector_thread = CollectorThread::spawn("ipc:///tmp/test.ipc");
+        it "should shut down after going out of scope" {
+            {
+                let _ = CollectorThread::spawn("ipc:///tmp/test-collector1.ipc");
+            }
+            assert!(true);
+        }
 
-            let mut collector = collector_thread.new_collector();
+        describe! collector {
+             it "should pass data points to nanosmg pull socket" {
+                let collector_thread = CollectorThread::spawn("ipc:///tmp/test-collector.ipc");
 
-            collector.collect("myserver", "os/cpu/usage", "user", DataValue::Float(0.4));
-            collector.collect("myserver", "os/cpu/usage", "user", DataValue::Float(0.4));
+                let mut collector = collector_thread.new_collector();
+
+                collector.collect("myserver", "os/cpu/usage", "user", DataValue::Float(0.4));
+                collector.collect("myserver", "os/cpu/usage", "user", DataValue::Float(0.4));
+
+                let mut msg = Vec::new();
+                match pull.read_to_end(&mut msg) {
+                    Ok(_) => println!("{:?}", msg),
+                    Err(error) => panic!("got error: {}", error)
+                }
+            }
         }
     }
 }
-*/
+
