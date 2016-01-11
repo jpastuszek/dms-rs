abc7e008681a3552f8a30301513d514cb5459a0d
Jakub Pastuszek <jpastuszek@gmail.com>
2016-01-11 22:21:57 +0000

    some initial work on threads and reorganisation

---
+++ src/collector/mod.rs
@@ -0,0 +1,150 @@
+use std::thread;
+use std::sync::mpsc::sync_channel;
+use std::sync::mpsc::{Receiver, SyncSender};
+
+use nanomsg::{Socket, Protocol};
+use chrono::{DateTime, UTC};
+
+use messaging::*;
+
+//TODO: rename
+pub struct CollectorThread {
+    sink: SyncSender<Box<RawDataPoint>>
+}
+
+impl CollectorThread {
+    pub fn spawn<S>(data_processor_address: S) -> CollectorThread where S: Into<String> {
+        let (tx, rx): (SyncSender<Box<RawDataPoint>>, Receiver<Box<RawDataPoint>>) = sync_channel(1000);
+
+        let local_data_processor_address = data_processor_address.into();
+        let _ = thread::spawn(move || {
+            let mut socket = Socket::new(Protocol::Push).ok().expect("Cannot create push socket!");
+            let mut _endpoint = match socket.connect(&local_data_processor_address) {
+                Ok(ep) => ep,
+                Err(error) => panic!("Failed to connect data processor at '{}': {}", local_data_processor_address, error)
+            };
+
+            loop {
+                // TODO: The mpsc::Receiver type can now be converted into an iterator with
+                // into_iter on the IntoIterator trait.
+                match rx.recv() {
+                    Ok(raw_data_point) => {
+                        socket.send_message("".to_string(), *raw_data_point, Encoding::Capnp).unwrap();
+                    },
+                    Err(error) => {
+                        info!("Collector thread finished: {}", error);
+                        return ();
+                    }
+                }
+            }
+        });
+
+        CollectorThread {
+            sink: tx
+        }
+    }
+
+    pub fn new_collector(&self) -> Collector {
+        Collector {
+            timestamp: UTC::now(),
+            sink: self.sink.clone(),
+        }
+    }
+}
+
+pub trait Collect {
+    fn collect(&mut self, location: &str, path: &str, component: &str, value: DataValue) -> ();
+}
+
+pub struct Collector {
+    timestamp: DateTime<UTC>,
+    sink: SyncSender<Box<RawDataPoint>>,
+}
+
+impl Collect for Collector {
+    fn collect(&mut self, location: &str, path: &str, component: &str, value: DataValue) -> () {
+        let raw_data_point = Box::new(RawDataPoint {
+            location: location.to_string(),
+            path: path.to_string(),
+            component: component.to_string(),
+            timestamp: self.timestamp,
+            value: value
+        });
+
+        match self.sink.send(raw_data_point) {
+            Ok(_) => {
+                debug!("Collected raw data point for location: '{}', path: '{}', component: '{}'", location, path, component);
+            }
+            Err(error) => {
+                error!("Failed to send collected raw data point: {}", error);
+            }
+        }
+    }
+}
+
+pub enum CollectorEvent {
+    Shutdown
+}
+
+mod probe;
+
+/*
+pub fn start<S>(data_processor_address: S) -> CollectorThread where S: Into<String> {
+    let collector_thread = CollectorThread::spawn(data_processor_address);
+
+    let probe = probe::start(collector_thread.new_collector());
+    //TODO: register modules
+    collector_thread.add(probe);
+
+    collector_thread
+}
+*/
+
+#[cfg(test)]
+mod test {
+    pub use super::*;
+    pub use messaging::*;
+    pub use nanomsg::{Socket, Protocol};
+    pub use std::io::Read;
+
+    mod collector_thread {
+        pub use super::*;
+        #[test]
+        fn should_shut_down_after_going_out_of_scope() {
+            {
+                let _ = CollectorThread::spawn("ipc:///tmp/test-collector1.ipc");
+            }
+            assert!(true);
+        }
+
+        mod collector {
+            pub use super::*;
+
+            #[test]
+             fn should_pass_data_points_to_nanosmg_pull_socket() {
+                let mut pull = Socket::new(Protocol::Pull).unwrap();
+                let mut _endpoint = pull.bind("ipc:///tmp/test-collector.ipc").unwrap();
+                {
+                    let collector_thread = CollectorThread::spawn("ipc:///tmp/test-collector.ipc");
+                    let mut collector = collector_thread.new_collector();
+
+                    collector.collect("myserver", "os/cpu/usage", "user", DataValue::Float(0.4));
+                    collector.collect("foobar", "os/cpu/sys", "user", DataValue::Float(0.4));
+
+                    let mut msg = Vec::new();
+                    pull.read_to_end(&mut msg).unwrap();
+                    let msg_string = String::from_utf8_lossy(&msg);
+                    assert!(msg_string.contains("RawDataPoint/\n0\ncapnp\n\n"));
+                    assert!(msg_string.contains("myserver"));
+
+                    let mut msg = Vec::new();
+                    pull.read_to_end(&mut msg).unwrap();
+                    let msg_string = String::from_utf8_lossy(&msg);
+                    assert!(msg_string.contains("RawDataPoint/\n0\ncapnp\n\n"));
+                    assert!(msg_string.contains("foobar"));
+                }
+            }
+        }
+    }
+}
+
---
+++ src/collector/probe/mod.rs
@@ -0,0 +1,378 @@
+use collector::{Collect, CollectorEvent};
+//use asynchronous::{Deferred, ControlFlow};
+use std::thread::{spawn, JoinHandle};
+use std::rc::Rc;
+use std::fmt;
+use std::error::Error;
+use time::Duration;
+use std::time::Duration as StdDuration;
+use token_scheduler::{Scheduler, SteadyTimeSource, WaitError};
+use std::sync::mpsc::channel;
+use std::sync::mpsc::{Receiver, Sender, SendError};
+use std::thread::sleep;
+
+#[allow(dead_code)]
+pub enum ProbeRunMode {
+    SharedThread,
+    //DedicatedThread,
+    //DedicatedProcess
+}
+
+pub struct ProbeSchedule<C> where C: Collect {
+    every: Duration,
+    probe: Rc<Probe<C>>
+}
+
+impl<C> Clone for ProbeSchedule<C> where C: Collect {
+    fn clone(&self) -> ProbeSchedule<C> {
+        ProbeSchedule {
+            every: self.every.clone(),
+            probe: self.probe.clone()
+        }
+    }
+}
+
+pub trait Probe<C>: Send where C: Collect {
+    fn name(&self) -> &str;
+    fn run(&self, collector: &mut C) -> Result<(), String>;
+    fn run_mode(&self) -> ProbeRunMode;
+}
+
+pub trait Module<C> where C: Collect {
+    fn name(&self) -> &str;
+    fn schedule(&self) -> Vec<ProbeSchedule<C>>;
+}
+
+pub struct SharedThreadProbeExecutor<C> where C: Collect {
+    probes: Vec<Rc<Probe<C>>>
+}
+
+impl<C> SharedThreadProbeExecutor<C> where C: Collect {
+    pub fn new() -> SharedThreadProbeExecutor<C> {
+        SharedThreadProbeExecutor {
+            probes: Vec::new()
+        }
+    }
+
+    pub fn push(&mut self, probe: Rc<Probe<C>>) {
+        self.probes.push(probe);
+    }
+
+    pub fn run(self, collector: &mut C) -> Vec<Result<(), String>> {
+        self.probes.into_iter().map(|probe| probe.run(collector)).collect()
+    }
+}
+
+pub struct ProbeScheduler<C> where C: Collect + {
+    scheduler: Scheduler<Rc<Probe<C>>, SteadyTimeSource>,
+    missed: usize
+}
+
+#[derive(Debug)]
+struct EmptySchedulerError;
+
+impl fmt::Display for EmptySchedulerError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "scheduler is empty")
+    }
+}
+
+impl Error for EmptySchedulerError {
+    fn description(&self) -> &str {
+        "probe schedule error"
+    }
+}
+
+impl<C> ProbeScheduler<C> where C: Collect {
+    pub fn new() -> ProbeScheduler<C> {
+        ProbeScheduler {
+            scheduler: Scheduler::new(Duration::milliseconds(100)),
+            missed: 0
+        }
+    }
+
+    pub fn schedule<'m>(&mut self, module: &'m Module<C>) {
+        for probe_schedule in module.schedule() {
+            self.scheduler.every(probe_schedule.every, probe_schedule.probe);
+        }
+    }
+
+    pub fn wait(&mut self) -> Result<Vec<Rc<Probe<C>>>, EmptySchedulerError> {
+         match self.scheduler.wait() {
+             Err(WaitError::Empty) => Err(EmptySchedulerError),
+             Err(WaitError::Missed(probe_runs)) => {
+                 //TODO: log missed
+                 self.missed = self.missed + probe_runs.len();
+                 self.wait()
+             },
+             Ok(probes) => {
+                 Ok(probes)
+             }
+         }
+    }
+
+    pub fn missed(&self) -> usize {
+        self.missed
+    }
+}
+
+struct Timer {
+    task_tx: Sender<(Timer, Sender<Timer>, Duration)>
+}
+
+//TODO: this self eating version is a bit complicated and slower than just exposing chanels
+// directly, but somehow safer to use since you need to recv() before you can send again
+impl Timer {
+    fn start() -> Timer {
+        let (task_tx, task_rx): (_, Receiver<(Timer, Sender<Timer>, Duration)>) = channel();
+
+        spawn(move || {
+            loop {
+                match task_rx.recv().map(|(timer, alarm_tx, duration)| {
+                        sleep(StdDuration::new(
+                            duration.num_seconds() as u64,
+                            (duration.num_nanoseconds().expect("sleep duration too large") - duration.num_seconds() * 1_000_000_000) as u32
+                        ));
+                        alarm_tx.send(timer)
+                }) {
+                    Ok(_) => (),
+                    Err(error) => {
+                        info!("Timer thread finished: {}", error);
+                        return ();
+                    }
+                }
+            }
+        });
+
+        Timer {
+            task_tx: task_tx
+        }
+    }
+
+    fn alarm_in(self, duration: Duration) -> Result<Receiver<Timer>, SendError<(Timer, Sender<Timer>, Duration)>> {
+        let task_tx = self.task_tx.clone();
+        //TODO: Need to crate channel every time since I cannot clone alarm_rx as above; can I move
+        //it out?
+        let (alarm_tx, alarm_rx) = channel();
+        task_tx.send((self, alarm_tx, duration)).map(|_| alarm_rx)
+    }
+}
+
+pub fn start<C>(collector: &C, events: Receiver<CollectorEvent>) -> JoinHandle<()> where C: Collect + Clone + Send + 'static {
+    let probe_collector = collector.clone();
+    spawn(move || {
+        let mut ps = ProbeScheduler::new();
+        let timer = Timer::start();
+
+        //TODO: load modules
+        //TODO: schedule modules
+        //TODO: need a way to stop the thread
+        // 1 use park_timeout as slee funciton and check some mutex or channel if we were asekd to
+        //   exit - this requires collector to send message or rais signal and then unpark
+        // 2 use Condvar with wait_timeout
+        // 3 use separate thread that will sleep and notify on channel when done - select on event
+        //   and it and use it to wait https://github.com/PeterReid/schedule_recv
+
+        loop {
+            let probes = ps.wait().expect("no probes configured to run");
+            let mut run_collector = probe_collector.clone();
+            let mut shared_exec = SharedThreadProbeExecutor::new();
+
+            for probe in probes {
+                match probe.run_mode() {
+                    ProbeRunMode::SharedThread => shared_exec.push(probe)
+                }
+            }
+
+            for error in shared_exec.run(&mut run_collector).into_iter().filter(|r| r.is_ok()).map(|r| r.unwrap()) {
+                //TODO: log run errors
+            }
+        }
+    })
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    use collector::Collect;
+    use messaging::DataValue;
+    use time::Duration;
+    use std::rc::Rc;
+
+    struct StubModule<C> where C: Collect {
+        name: String,
+        schedule: Vec<ProbeSchedule<C>>
+    }
+
+    struct StubProbe {
+        name: String
+    }
+
+    impl StubProbe {
+        fn new(name: &str) -> Rc<Self> {
+            Rc::new(
+                StubProbe {
+                    name: name.to_string()
+                }
+            )
+        }
+    }
+
+    impl<C> Probe<C> for StubProbe where C: Collect {
+        fn name(&self) -> &str {
+            &self.name
+        }
+
+        fn run(&self, collector: &mut C) -> Result<(), String> {
+            let mut collector = collector;
+            collector.collect("foo", &self.name, "c1", DataValue::Text(format!("{}-{}", self.name, "c1")));
+            collector.collect("bar", &self.name, "c2", DataValue::Text(format!("{}-{}", self.name, "c2")));
+            Ok(())
+        }
+
+        fn run_mode(&self) -> ProbeRunMode {
+            ProbeRunMode::SharedThread
+        }
+    }
+
+    impl<C> StubModule<C> where C: Collect  {
+        fn new(name: &str) -> StubModule<C> {
+            StubModule {
+                name: name.to_string(),
+                schedule: Vec::new()
+            }
+        }
+
+        fn add_schedule(&mut self, every: Duration, probe: Rc<Probe<C>>) {
+            self.schedule.push(
+                ProbeSchedule {
+                    every: every,
+                    probe: probe
+                }
+            );
+        }
+    }
+
+    impl<C> Module<C> for StubModule<C> where C: Collect {
+        fn name(&self) -> &str {
+            &self.name
+        }
+
+        fn schedule(&self) -> Vec<ProbeSchedule<C>> {
+            self.schedule.clone()
+        }
+    }
+
+    struct StubCollector {
+        pub values: Vec<DataValue>
+    }
+
+    impl Collect for StubCollector {
+        fn collect(&mut self, _location: &str, _path: &str, _component: &str, value: DataValue) -> () {
+            self.values.push(value);
+        }
+    }
+
+    impl StubCollector {
+        fn text_values(self) -> Vec<String> {
+            self.values.into_iter().map(|v| if let DataValue::Text(c) = v { c } else { "none".to_string() }).collect()
+        }
+    }
+
+    #[test]
+    fn shared_thread_probe_executor() {
+        let p1 = StubProbe::new("p1");
+        let p2 = StubProbe::new("p2");
+        let p3 = StubProbe::new("p3");
+
+        let mut exec = SharedThreadProbeExecutor::new();
+        exec.push(p1);
+        exec.push(p2);
+        exec.push(p3);
+
+        let mut collector = StubCollector { values: Vec::new() };
+        exec.run(&mut collector);
+
+        assert_eq!(collector.text_values(), vec![
+           "p1-c1", "p1-c2",
+           "p2-c1", "p2-c2",
+           "p3-c1", "p3-c2"
+        ]);
+    }
+
+    #[test]
+    fn probe_scheduler_wait_should_provide_porbes_according_to_schedule() {
+        let mut m1 = StubModule::new("m1");
+        m1.add_schedule(Duration::milliseconds(100), StubProbe::new("m1-p1"));
+
+        let mut m2 = StubModule::new("m2");
+        m2.add_schedule(Duration::milliseconds(100), StubProbe::new("m2-p1"));
+        m2.add_schedule(Duration::milliseconds(200), StubProbe::new("m2-p2"));
+
+
+        let mut ps: ProbeScheduler<StubCollector> = ProbeScheduler::new();
+        ps.schedule(&m1);
+        ps.schedule(&m2);
+
+        let result = ps.wait();
+        assert!(result.is_ok());
+
+        let mut collector = StubCollector { values: Vec::new() };
+        let probes = result.unwrap();
+        for probe in probes {
+            probe.run(&mut collector).unwrap();
+        }
+
+        assert_eq!(collector.text_values(), vec![
+           "m1-p1-c1", "m1-p1-c2",
+           "m2-p1-c1", "m2-p1-c2"
+        ]);
+    }
+
+    #[test]
+    fn probe_scheduler_wait_should_count_missed_schedules() {
+        use std::thread::sleep_ms;
+
+        let mut m1 = StubModule::new("m1");
+        m1.add_schedule(Duration::milliseconds(100), StubProbe::new("m1-p1"));
+        m1.add_schedule(Duration::milliseconds(100), StubProbe::new("m1-p2"));
+
+        let mut m2 = StubModule::new("m2");
+        m2.add_schedule(Duration::milliseconds(200), StubProbe::new("m2-p1"));
+
+        let mut ps: ProbeScheduler<StubCollector> = ProbeScheduler::new();
+        ps.schedule(&m1);
+        ps.schedule(&m2);
+
+        sleep_ms(200);
+
+        {
+            let result = ps.wait();
+            assert!(result.is_ok());
+
+            let mut collector = StubCollector { values: Vec::new() };
+            let probes = result.unwrap();
+            for probe in probes {
+                probe.run(&mut collector).unwrap();
+            }
+
+            assert_eq!(collector.text_values(), vec![
+               "m2-p1-c1", "m2-p1-c2",
+               "m1-p1-c1", "m1-p1-c2",
+               "m1-p2-c1", "m1-p2-c2"
+            ]);
+        }
+
+        assert_eq!(ps.missed(), 2);
+    }
+
+    #[test]
+    fn timer() {
+        use super::Timer;
+
+        let mut timer = Timer::start();
+        timer = timer.alarm_in(Duration::milliseconds(10)).unwrap().recv().unwrap();
+        assert!(timer.alarm_in(Duration::milliseconds(10)).unwrap().recv().is_ok());
+    }
+}
+
